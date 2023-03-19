97a3d155e24bc658609650f3f82eeda63ae7ee94
friendywill <friendywill@hotmail.com>
2023-03-19 00:44:13 +0000

    modified:   Configuration/named.conf 	modified:   docker-compose.yml 	new file:   hosttocont.sh

---
+++ hosttocont.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#Change these variables!
+NIC_NAME="enp1s0"
+DOCKER_ROUTING_INTERFACE_NAME="dockerrouteif"
+DOCKERNETWORK_IP_ADDRESS="192.168.0.11/32"
+DOCKERNETWORK_IP_RANGE="192.168.0.0/24"
+SLEEP_TIME="15" #seconds
+
+sleep ${SLEEP_TIME} #Do not rush things if executing during boot. This line is not mandatory and can be removed.
+
+
+ip link add ${DOCKER_ROUTING_INTERFACE_NAME} link ${NIC_NAME} type macvlan mode bridge ; ip addr add ${DOCKERNETWORK_IP_ADDRESS} dev ${DOCKER_ROUTING_INTERFACE_NAME} ; ip link set ${DOCKER_ROUTING_INTERFACE_NAME} up
+ip route add ${DOCKERNETWORK_IP_RANGE} dev ${DOCKER_ROUTING_INTERFACE_NAME}
